d347d939e8090581ae4293b483551152716c06d8
cahangeorge <cahangeorge@gmail.com>
2026-01-28 16:42:58 +0200

    Revert "feat: add U5-U23 age categories"
    This reverts commit 8f9adc7831ac760d6cf5dc114a6bd9287050b877.

--- src/common/enums/index.ts
+++ src/common/enums/index.ts
@@ -56,25 +56,13 @@ export enum Currency {
 }
 
 export enum AgeCategory {
-  U5 = 'U5',
-  U6 = 'U6',
-  U7 = 'U7',
   U8 = 'U8',
-  U9 = 'U9',
   U10 = 'U10',
-  U11 = 'U11',
   U12 = 'U12',
-  U13 = 'U13',
   U14 = 'U14',
-  U15 = 'U15',
   U16 = 'U16',
-  U17 = 'U17',
   U18 = 'U18',
-  U19 = 'U19',
-  U20 = 'U20',
   U21 = 'U21',
-  U22 = 'U22',
-  U23 = 'U23',
   SENIOR = 'SENIOR',
   VETERANS = 'VETERANS',
 }
